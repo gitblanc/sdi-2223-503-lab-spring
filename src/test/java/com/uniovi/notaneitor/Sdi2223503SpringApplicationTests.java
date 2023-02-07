5f03d5deea0ac297ce42e4e3a5bd34f737f19f25
gitblanc <UO285176@uniovi.es>
2023-02-07 14:38:11 +0100

    SDI-2223-503-2.1-Create Spring project.

---
+++ src/test/java/com/uniovi/notaneitor/Sdi2223503SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaneitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223503SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
