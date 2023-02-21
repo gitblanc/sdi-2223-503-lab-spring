6b1531d60a0560bef587cf04f8ab34395a319e1a
gitblanc <UO285176@uniovi.es>
2023-02-21 17:35:44 +0100

    SDI-2223-503-4.4-SpringBoot-Consulta

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -1,6 +1,7 @@
 package com.uniovi.notaneitor.controllers;
 
 import com.uniovi.notaneitor.entities.Mark;
+import com.uniovi.notaneitor.entities.User;
 import com.uniovi.notaneitor.services.MarksService;
 import com.uniovi.notaneitor.services.UsersService;
 import com.uniovi.notaneitor.validators.AddMarksFormValidator;
@@ -12,6 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,15 +33,19 @@ public class MarksController {
     private HttpSession httpSession;
 
     @RequestMapping("/mark/list")
-    public String getList(Model model) {
-        model.addAttribute("markList", marksService.getMarks());
+    public String getList(Model model, Principal principal) {
+        String dni = principal.getName(); // DNI es el name de la autenticación
+        User user = usersService.getUserByDni(dni);
+        model.addAttribute("markList", marksService.getMarksForUser(user));
         return "mark/list";
     }
 
     @RequestMapping("/mark/list/update")
-    public String updateList(Model model) {
-        model.addAttribute("markList", marksService.getMarks());
-        return "mark/list::tableMarks";
+    public String updateList(Model model, Principal principal) {
+        String dni = principal.getName(); // DNI es el name de la autenticación
+        User user = usersService.getUserByDni(dni);
+        model.addAttribute("markList", marksService.getMarksForUser(user));
+        return "mark/list :: tableMarks";
     }
 
     @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
--- src/main/java/com/uniovi/notaneitor/repositories/MarksRepository.java
+++ src/main/java/com/uniovi/notaneitor/repositories/MarksRepository.java
@@ -1,14 +1,20 @@
 package com.uniovi.notaneitor.repositories;
 
 import com.uniovi.notaneitor.entities.Mark;
+import com.uniovi.notaneitor.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MarksRepository extends CrudRepository<Mark, Long> {
     @Modifying
     @Transactional
     @Query("UPDATE Mark SET resend = ?1 WHERE id = ?2")
     void updateResend(Boolean resend, Long id);
+
+    @Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC")
+    List<Mark> findAllByUser(User user);
 }
